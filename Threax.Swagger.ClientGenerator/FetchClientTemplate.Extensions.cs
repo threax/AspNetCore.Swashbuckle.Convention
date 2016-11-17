6541d35c014fccc8c3876c382997a9be7184bd4d
Andrew Piper <piper.andrew@spcollege.edu>
2016-11-17 09:02:07 -0500

    Added a client generator program. This has to be regular .net since it uses templates like nswag, when they have a .net core solution or if one can be figured out, this will change.

---
+++ Threax.Swagger.ClientGenerator/FetchClientTemplate.Extensions.cs
@@ -0,0 +1,20 @@
+﻿using NJsonSchema.CodeGeneration;
+using NSwag.CodeGeneration.CodeGenerators.TypeScript.Models;
+
+namespace Threax.Swagger.ClientGenerator
+{
+    internal partial class FetchClientTemplate : ITemplate
+    {
+        public FetchClientTemplate(ClientTemplateModel model)
+        {
+            Model = model;
+        }
+
+        public ClientTemplateModel Model { get; }
+        
+        public string Render()
+        {
+            return ConversionUtilities.TrimWhiteSpaces(TransformText());
+        }
+    }
+}
---
+++ Threax.Swagger.ClientGenerator/Program.cs
@@ -0,0 +1,66 @@
+﻿using NJsonSchema.CodeGeneration;
+using NJsonSchema.CodeGeneration.TypeScript;
+using NSwag;
+using NSwag.CodeGeneration.CodeGenerators;
+using NSwag.CodeGeneration.CodeGenerators.TypeScript;
+using NSwag.Commands;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Threax.Swagger.ClientGenerator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Task.WaitAll(AsyncMain(args));
+
+        }
+
+        static async Task AsyncMain(String[] args)
+        { 
+            string documentPath = args[0];
+            string swaggerData;
+
+            if (args.Length > 1)
+            {
+                swaggerData = args[1];
+            }
+            else
+            {
+                if(documentPath.StartsWith("http", StringComparison.OrdinalIgnoreCase))
+                {
+                    using (HttpClient httpClient = new HttpClient())
+                    {
+                        swaggerData = await httpClient.GetStringAsync(documentPath);
+                    }
+                }
+                else
+                {
+                    using(StreamReader sr = new StreamReader(File.Open(documentPath, FileMode.Open, FileAccess.Read, FileShare.Read)))
+                    {
+                        swaggerData = await sr.ReadToEndAsync();
+                    }
+                }
+            }
+
+            SwaggerToTypeScriptClientCommand Command = new SwaggerToTypeScriptClientCommand();
+            Command.Settings.TypeScriptGeneratorSettings.TemplateFactory = new FetchTemplateFactory(Command.Settings.TypeScriptGeneratorSettings.TemplateFactory);
+            Command.OperationGenerationMode = OperationGenerationMode.MultipleClientsFromPathSegments;
+            Command.Template = TypeScriptTemplate.Fetch;
+            Command.TypeStyle = TypeScriptTypeStyle.Interface;
+            Command.Input = SwaggerDocument.FromJson(swaggerData, documentPath);
+            var code = await Command.RunAsync();
+
+            using (var sr = new StreamWriter(File.Open("test.ts", FileMode.Create, FileAccess.Write, FileShare.None)))
+            {
+                await sr.WriteAsync(code);
+            }
+        }
+    }
+}
