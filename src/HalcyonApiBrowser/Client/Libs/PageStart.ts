3352f7ae51c0b5901cd750698ff89ee38b1df8aa
Andrew Piper <piper.andrew@spcollege.edu>
2016-11-30 09:35:04 -0500

    Added a project to act as a generic api browser for our halcyon convention to test how it works. It should also be possible to browse any conventional api using this website.

---
+++ src/HalcyonApiBrowser/Client/Libs/PageStart.ts
@@ -0,0 +1,21 @@
+﻿import * as bootstrap from 'hr.bootstrap.all';
+
+export class PageStart {
+    constructor() {
+        //Activate bootstrap here, this way we know its active and has registered all its models and toggles when starting up other controllers.
+        bootstrap.activate();
+    }
+}
+
+var instance: PageStart = null;
+/**
+ * Set up common config for the page to run.
+ * @returns A Promise with the PageStart instance inside.
+ */
+export function init(): Promise<PageStart> {
+    if (instance === null) {
+        instance = new PageStart();
+    }
+
+    return Promise.resolve(instance);
+}
