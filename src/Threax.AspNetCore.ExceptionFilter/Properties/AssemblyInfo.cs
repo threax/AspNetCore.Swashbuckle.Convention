7d2b057bc1b5a017723b92ff300f191b28d419d3
Andrew Piper <piper.andrew@spcollege.edu>
2017-01-31 11:58:47 -0500

    Renaming ExceptionToJson to ExceptionFilter since it really changes exceptions to objectresults, which are then handled by the output formatters to get their final form.

--- src/Threax.AspNetCore.ExceptionFilter/Properties/AssemblyInfo.cs
+++ src/Threax.AspNetCore.ExceptionFilter/Properties/AssemblyInfo.cs
@@ -7,7 +7,7 @@ using System.Runtime.InteropServices;
 // associated with an assembly.
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("Threax.AspNetCore.ExceptionToJson")]
+[assembly: AssemblyProduct("Threax.AspNetCore.ExceptionFilter")]
 [assembly: AssemblyTrademark("")]
 
 // Setting ComVisible to false makes the types in this assembly not visible
@@ -16,4 +16,4 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("5b8700b7-8c1e-44ae-807c-64dc1c7ba7d7")]
+[assembly: Guid("0f437156-e559-4749-8ea9-e788daf07f74")]
