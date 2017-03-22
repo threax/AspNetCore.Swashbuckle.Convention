33cd59091646f794b0167e063e5f86b309ca674f
Andrew Piper <piper.andrew@spcollege.edu>
2017-03-22 13:44:08 -0400

    Changed EnumValueProvider to LabelValuePairProvider.

--- src/TestHalcyonApi/ValueProviders/OtherWeirdThingProvider.cs
+++ src/TestHalcyonApi/ValueProviders/OtherWeirdThingProvider.cs
@@ -6,17 +6,17 @@ using Threax.AspNetCore.Halcyon.Ext.ValueProviders;
 
 namespace TestHalcyonApi.ValueProviders
 {
-    public class OtherWeirdThingProvider : EnumValueProvider
+    public class OtherWeirdThingProvider : LabelValuePairProvider
     {
         public OtherWeirdThingProvider()
         {
         }
 
-        protected override IEnumerable<EnumValue> GetSources()
+        protected override IEnumerable<LabelValuePair> GetSources()
         {
-            yield return new EnumValue("Value 1", "5f959548-7edc-4c6d-970c-ff2ea44e62c3");
-            yield return new EnumValue("Value 2", "7dfbbd59-645b-4f59-9bf2-287a4fcfd023");
-            yield return new EnumValue("Value 3", "c34a78dc-3192-4ca4-a12a-ada902c94a93");
+            yield return new LabelValuePair("Value 1", "5f959548-7edc-4c6d-970c-ff2ea44e62c3");
+            yield return new LabelValuePair("Value 2", "7dfbbd59-645b-4f59-9bf2-287a4fcfd023");
+            yield return new LabelValuePair("Value 3", "c34a78dc-3192-4ca4-a12a-ada902c94a93");
         }
     }
 }
