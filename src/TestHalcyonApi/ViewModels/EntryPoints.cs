fd7829567b17193f8ef0e1aa4ce5719e0cc23116
Andrew Piper <piper.andrew@spcollege.edu>
2017-04-05 15:59:10 -0400

    Added a way to mark a link as docs only, which means it only provides doc links.

--- src/TestHalcyonApi/ViewModels/EntryPoints.cs
+++ src/TestHalcyonApi/ViewModels/EntryPoints.cs
@@ -14,6 +14,7 @@ namespace TestHalcyonApi.ViewModels
     [HalSelfActionLink(EntryPointController.Rels.Get, typeof(EntryPointController))]
     [HalActionLink(ThingyController.Rels.List, typeof(ThingyController))]
     [HalActionLink(MultipartInputController.Rels.BeginAddMultipart, MultipartInputController.Rels.GetPart1, typeof(MultipartInputController))] //Here we bind a rel always named BeginAddMultipart to the real first step, this way we can alter the first step without changing the ui code.
+    [HalActionLink(ThingyController.Rels.Update, typeof(ThingyController), DocsOnly = true)] //Test out sending only docs for the thingy update, this kind of thing is useful if you have a crud table that does not allow adds
     public class EntryPoints
     {
     }
