2db6c4ed44c0c180b03092394181f19896bb1883
Andrew Piper <piper.andrew@spcollege.edu>
2017-05-10 13:16:39 -0400

    Added a way to automaticaly handle enums with a value provider instead of the built in njsonschema way.

--- src/TestHalcyonApi/Models/SubThingy.cs
+++ src/TestHalcyonApi/Models/SubThingy.cs
@@ -1,10 +1,21 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 
 namespace TestHalcyonApi.Models
 {
+    public enum TestEnum
+    {
+        [Display(Name = "Test Value 1")]
+        TestValue1,
+        [Display(Name = "Test Value 2")]
+        TestValue2,
+        [Display(Name = "Test Value 3")]
+        TestValue3,
+    }
+
     public class SubThingy
     {
         public int SubThingyId { get; set; }
@@ -12,5 +23,7 @@ namespace TestHalcyonApi.Models
         public decimal Amount { get; set; }
 
         public int ThingyId { get; set; }
+
+        public TestEnum EnumTest { get; set; }
     }
 }
