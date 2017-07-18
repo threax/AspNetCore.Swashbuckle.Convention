ac17a43204d10fdbbca8628ae1e96e600ed0c331
Andrew Piper <piper.andrew@spcollege.edu>
2017-07-18 13:51:01 -0400

    Changed value providers to use an interface, able to return any type for value now. Breaks compilation, version bumped to 4.

--- src/TestHalcyonApi/ValueProviders/OtherWeirdThingProvider.cs
+++ src/TestHalcyonApi/ValueProviders/OtherWeirdThingProvider.cs
@@ -12,11 +12,12 @@ namespace TestHalcyonApi.ValueProviders
         {
         }
 
-        protected override IEnumerable<LabelValuePair> GetSourcesSync()
+        protected override IEnumerable<ILabelValuePair> GetSourcesSync()
         {
             yield return new LabelValuePair("Value 1", "5f959548-7edc-4c6d-970c-ff2ea44e62c3");
             yield return new LabelValuePair("Value 2", "7dfbbd59-645b-4f59-9bf2-287a4fcfd023");
             yield return new LabelValuePair("Value 3", "c34a78dc-3192-4ca4-a12a-ada902c94a93");
+            yield return new LabelValuePair<int>("Value 4", 4);
         }
     }
 }
